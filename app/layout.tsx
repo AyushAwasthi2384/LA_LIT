df7a80930c3c752f20be4cb37fc4e65812fe751a
AYUSH AWASTHI�� <97173127+AyushAwasthi2384@users.noreply.github.com>
2025-01-22 10:16:29 +0530

    git fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  return (
+  return ( 
     <html lang="en">
       <ToastProvider>
         <body className={inter.className}>
